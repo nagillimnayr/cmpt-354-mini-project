500d27573cb327860789a0927ce618b15438cbeb
Ryan Milligan <rcm9@sfu.ca>
2025-03-16 14:31:33 -0700

    move notebook to notebook folder

--- sample_data/insert_sample_data.py
+++ sample_data/insert_sample_data.py
@@ -4,11 +4,15 @@ import os
 connection = sqlite3.connect('library.db')
 cursor = connection.cursor()
 
-
-
 def insert_sample_data():
-
-
+  """ 
+  Reads in SQL INSERT commands from files and executes them. 
+  """
+  files = os.listdir()
+  for file_name in files:
+    with open(f'schemas/{file_name}') as file:
+      command = file.read()
+      cursor.execute(command)
 
 def main():
   insert_sample_data()
