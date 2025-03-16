03b80f31c99428ccab86891bc8c6538884893091
Ryan Milligan <rcm9@sfu.ca>
2025-03-16 16:30:20 -0700

    add trigger

--- main.py
+++ main.py
@@ -14,7 +14,26 @@ def create_tables():
     with open(f'{dir}/{file_name}') as file:
       command = file.read()
       cursor.execute(command)
-      
+  
+  """
+  We can't use subqueries inside `CHECK` or `ASSERTION` statements, so we must
+  use a `TRIGGER` to impose constraint on number of `OverdueFine`s for a single
+  `CheckoutRecord`.
+  """
+  cursor.execute("""
+    CREATE TRIGGER IF NOT EXISTS max_overdue_fines_per_checkout 
+    BEFORE INSERT ON OverdueFine 
+    WHEN (
+        SELECT COUNT(*)
+        FROM OverdueFine AS O
+        WHERE O.checkoutId = NEW.checkoutId
+      ) >= 10
+    BEGIN
+        SELECT 
+        RAISE (ABORT, 'Maximum number of fines reached for this Checkout');
+    END;
+    """
+  )
 
 def main():
   create_tables()
