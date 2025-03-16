360ab6fd93971bd9d60cccf1f24540d97cdf0d12
Ryan Milligan <rcm9@sfu.ca>
2025-03-16 14:06:07 -0700

    read .sql files in python script

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 import sqlite3
+import os
 
+connection = sqlite3.connect('library.db')
+cursor = connection.cursor()
+
+def create_tables():
+  """ 
+  Reads in SQL commands from files in schemas/ folder and executes them. 
+  """
+  schema_files = os.listdir('schemas')
+  for file_name in schema_files:
+    with open(f'schemas/{file_name}') as file:
+      command = file.read()
+      cursor.execute(command)
+      
+    
 
 def main():
-  connection = sqlite3.connect('library.db')
-  cursor = connection.cursor()
-  
-  # TODO: Create Tables
+  create_tables()
   
 if __name__ == '__main__':
   main()
