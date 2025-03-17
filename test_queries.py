42fc8dca12f23944dece726f5feb1c0c2ac4fdc9
Carson James <Carsonjames@shaw.ca>
2025-03-17 15:47:13 -0700

    fixed borrow_item workflow and implemented return_item

--- test_queries.py
+++ test_queries.py
@@ -1,18 +1,28 @@
 import sqlite3
 
 from borrow_item import borrow_item
+from return_item import return_item
 from constants import DB_PATH
 
 
 def test_queries():
     
-  borrow_item(member_id=3, item_id=5, librarian_id=2)
+  #borrow_item(member_id=3, item_id=5, librarian_id=2)
+  return_item(item_id=5, instance_id=6)
 
   with sqlite3.connect(DB_PATH) as connection:
     cursor = connection.cursor()
     cursor.execute("""
       SELECT * FROM CheckoutRecord
       WHERE memberId = 3;  
+      """
+    )
+    result = cursor.fetchall()
+    print('\n')
+    print(result)
+    cursor.execute("""
+      SELECT currentCheckoutId FROM ItemInstance
+      WHERE itemId = 5;
       """
     )
     result = cursor.fetchall()
