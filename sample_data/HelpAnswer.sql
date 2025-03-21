925748a885b15b668bb99e453f8002394e5148dd
Ryan Milligan <rcm9@sfu.ca>
2025-03-20 17:03:52 -0700

    fix insert

--- sample_data/HelpAnswer.sql
+++ sample_data/HelpAnswer.sql
@@ -1,16 +1,8 @@
-INSERT INTO HelpAnswer (
-  answerId,
-  personnelId,
-  answer,
-  datePublished,
-)
-
-;
 INSERT INTO HelpAnswer (answerId, questionId, personnelId, answer, datePublished)
 VALUES 
   (1, 1, 2, "Herman Melville.", "2024-05-24"),
   (2, 2, 3, "'To Kill a Mockingbird' was published in 1960.", "2024-06-11"),
   (3, 2, 2, "It was published on July 11th, 1960.", "2024-06-17"),
-  (4, 3, 3, "Christopher Nolan.", "2024-06-18")
+  (4, 3, 3, "Christopher Nolan.", "2024-06-18"),
   (5, 4, 3, "Yes, we do.", "2025-03-24")
 ;
