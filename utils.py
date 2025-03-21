ef295ecf2d6acebfb267f8cf1b78db06613af71e
Ryan Milligan <rcm9@sfu.ca>
2025-03-20 17:24:06 -0700

    add dict_row_factory()

--- utils.py
+++ utils.py
@@ -11,3 +11,10 @@ def pretty_print(obj):
 
 def get_current_date():
     return datetime.now().strftime("%Y-%m-%d")
+
+def dict_row_factory(cursor, row):
+    """
+    https://docs.python.org/3/library/sqlite3.html#sqlite3-howto-row-factory
+    """
+    fields = [column[0] for column in cursor.description]
+    return {key: value for key, value in zip(fields, row)}
