24eb47da953b8a2bade2617d9989221ea6cddd96
Carson James <Carsonjames@shaw.ca>
2025-03-21 22:04:55 -0700

    fixed instance numbers

--- sample_data/ItemInstance.sql
+++ sample_data/ItemInstance.sql
@@ -2,16 +2,16 @@ INSERT INTO ItemInstance (instanceId, itemId, currentCheckoutId)
 VALUES
   (1, 1, NULL), -- The Great Gatsby, available
   (2, 1, NULL), -- The Great Gatsby, second copy
-  (3, 2, NULL), -- Inception, available
-  (4, 3, NULL), -- National Geographic - July Edition, available
-  (5, 4, NULL), -- Python Programming, available
-  (6, 5, NULL), -- Beethoven Symphony No.9, available
-  (7, 6, NULL), -- To Kill a Mockingbird, available
-  (8, 7, NULL), -- The Lord of the Rings, available
-  (9, 8, NULL), -- 1984, available
-  (10, 9, NULL), -- The Matrix, available
-  (11, 10, NULL), -- Interstellar, available
-  (12, 11, NULL), -- Dune, available
-  (13, 12, NULL), -- The Catcher in the Rye, available
-  (14, 13, NULL), -- Cosmos, available
-  (15, 14, NULL); -- The Art of War, available
+  (1, 2, NULL), -- Inception, available
+  (1, 3, NULL), -- National Geographic - July Edition, available
+  (1, 4, NULL), -- Python Programming, available
+  (1, 5, NULL), -- Beethoven Symphony No.9, available
+  (1, 6, NULL), -- To Kill a Mockingbird, available
+  (1, 7, NULL), -- The Lord of the Rings, available
+  (1, 8, NULL), -- 1984, available
+  (1, 9, NULL), -- The Matrix, available
+  (1, 10, NULL), -- Interstellar, available
+  (1, 11, NULL), -- Dune, available
+  (1, 12, NULL), -- The Catcher in the Rye, available
+  (1, 13, NULL), -- Cosmos, available
+  (1, 14, NULL); -- The Art of War, available
