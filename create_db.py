085269ff1f52010b84e3f57429d2a1e9b7a3d9d5
Ryan Milligan <rcm9@sfu.ca>
2025-03-27 16:18:17 -0700

    consolidate creation of db

--- create_db.py
+++ create_db.py
@@ -3,6 +3,50 @@ import os
 
 from constants import DB_PATH
 
+
+TABLES = [
+  'Audience',
+  'CheckoutRecord',
+  'Event',
+  'EventAttendance',
+  'EventRecommendation',
+  'HelpAnswer',
+  'HelpQuestion',
+  'Item',
+  'ItemInstance',
+  'Member',
+  'MemberAudienceType',
+  'OverdueFine',
+  'Personnel',
+  'SocialRoom',
+]
+DROP_TABLE_COMMANDS = ["DROP TABLE IF EXISTS " + table + ";" for table in TABLES]
+
+def drop_tables():
+  with sqlite3.connect(DB_PATH) as connection:
+    cursor = connection.cursor()
+    for cmd in DROP_TABLE_COMMANDS:
+      cursor.execute(cmd)
+
+VIEWS = [
+  'HelpAnswerView',
+  'HelpQuestionView',
+  'PersonnelInfo',
+  'PersonnelView',
+]   
+DROP_VIEW_COMMANDS = ["DROP VIEW IF EXISTS " + view + ";" for view in VIEWS]
+
+def drop_views():
+  with sqlite3.connect(DB_PATH) as connection:
+    cursor = connection.cursor()
+    for cmd in DROP_VIEW_COMMANDS:
+      cursor.execute(cmd)
+
+def drop_all():
+  drop_tables()    
+  drop_views()
+  
+
 def execute_sql_in_directory(dir_name: str):
   """
   Reads all .sql files from a directory and executes the commands within.
@@ -14,7 +58,6 @@ def execute_sql_in_directory(dir_name: str):
       with open(f'{dir_name}/{file_name}') as file:
         command = file.read()
         cursor.execute(command)
-        
 
 
 def create_tables():
@@ -56,9 +99,62 @@ def create_database():
   create_views()
   create_triggers()
 
+def insert_sample_data():
+  """ 
+  Reads in SQL INSERT commands from files and executes them. 
+  Commands need to be executed in a specific order.
+  """
+  
+  with sqlite3.connect(DB_PATH) as connection:
+    cursor = connection.cursor()
+    dir = 'sample_data'
+    file_names = os.listdir(dir)
+    for file_name in file_names:
+      with open(f'{dir}/{file_name}') as file:
+        command = file.read()
+        cursor.execute(command)
+        
+    updates = [
+      """
+      UPDATE ItemInstance
+      SET currentCheckoutId = 3 
+      WHERE instanceId = 5;
+      """,
+      """
+      UPDATE ItemInstance
+      SET currentCheckoutId = 4 
+      WHERE instanceId = 7;
+      """,
+      """
+      UPDATE ItemInstance
+      SET currentCheckoutId = 5 
+      WHERE instanceId = 9;
+      """,
+      """
+      UPDATE ItemInstance
+      SET currentCheckoutId = 6 
+      WHERE instanceId = 11;
+      """,
+      """
+      UPDATE ItemInstance
+      SET currentCheckoutId = 7 
+      WHERE instanceId = 13;
+      """,
+      """
+      UPDATE ItemInstance
+      SET currentCheckoutId = 8 
+      WHERE instanceId = 15;
+      """,
+    ]
+    
+    for update in updates:
+      cursor.execute(update)
+      
 
 if __name__ == '__main__':
   try:
+    drop_all()
     create_database()
+    insert_sample_data()
   except sqlite3.Error as e:
     print(e)
