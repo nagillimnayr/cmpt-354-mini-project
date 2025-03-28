a687c96cc455d4e6c66389d4eae93c16b31814ec
Ryan Milligan <rcm9@sfu.ca>
2025-03-28 13:33:06 -0700

    add triggers for checkout records

--- create_db.py
+++ create_db.py
@@ -44,7 +44,7 @@ def drop_views():
 
 def drop_all():
   drop_tables()    
-  # drop_views()
+  drop_views()
   
 
 def execute_sql_in_directory(dir_name: str):
@@ -72,8 +72,9 @@ def create_views():
 
 
 def create_triggers():
-  with sqlite3.connect(DB_PATH) as connection:
-    cursor = connection.cursor()
+  conn = sqlite3.connect(DB_PATH) 
+  with conn:
+    cursor = conn.cursor()
     """
     We can't use subqueries inside `CHECK` or `ASSERTION` statements, so we must
     use a `TRIGGER` to impose constraint on number of `OverdueFine`s for a single
@@ -122,14 +123,8 @@ def create_triggers():
     cursor.execute("""
       CREATE TRIGGER IF NOT EXISTS set_current_checkout_id 
       AFTER INSERT ON CheckoutRecord
-      WHEN (
-        SELECT currentCheckoutId 
-        FROM ItemInstance
-        WHERE 
-          itemId = NEW.itemId 
-          AND
-          instanceId = NEW.instanceId
-      ) IS NULL
+      WHEN 
+        NEW.returnDate IS NULL        
       BEGIN 
         UPDATE ItemInstance
         SET currentCheckoutId = NEW.checkoutId
@@ -139,6 +134,7 @@ def create_triggers():
           instanceId = NEW.instanceId;
       END;
     """)
+    
     
     """
     Trigger to set `ItemInstance`'s `currentCheckoutRecord` to `NULL` when an 
@@ -158,6 +154,7 @@ def create_triggers():
           instanceId = NEW.instanceId;
       END;
     """)
+    
 
 def create_database():
   create_tables()
@@ -169,57 +166,24 @@ def insert_sample_data():
   Reads in SQL INSERT commands from files and executes them. 
   Commands need to be executed in a specific order.
   """
-  
-  with sqlite3.connect(DB_PATH) as connection:
-    cursor = connection.cursor()
+  with sqlite3.connect(DB_PATH) as conn:
+    cursor = conn.cursor()
     dir = 'sample_data'
     file_names = os.listdir(dir)
     for file_name in file_names:
       with open(f'{dir}/{file_name}') as file:
         command = file.read()
         cursor.execute(command)
-        
-    # updates = [
-    #   """
-    #   UPDATE ItemInstance
-    #   SET currentCheckoutId = 3 
-    #   WHERE instanceId = 5;
-    #   """,
-    #   """
-    #   UPDATE ItemInstance
-    #   SET currentCheckoutId = 4 
-    #   WHERE instanceId = 7;
-    #   """,
-    #   """
-    #   UPDATE ItemInstance
-    #   SET currentCheckoutId = 5 
-    #   WHERE instanceId = 9;
-    #   """,
-    #   """
-    #   UPDATE ItemInstance
-    #   SET currentCheckoutId = 6 
-    #   WHERE instanceId = 11;
-    #   """,
-    #   """
-    #   UPDATE ItemInstance
-    #   SET currentCheckoutId = 7 
-    #   WHERE instanceId = 13;
-    #   """,
-    #   """
-    #   UPDATE ItemInstance
-    #   SET currentCheckoutId = 8 
-    #   WHERE instanceId = 15;
-    #   """,
-    # ]
+  
     
-    # for update in updates:
-    #   cursor.execute(update)
-      
 
 if __name__ == '__main__':
   try:
     drop_all()
     create_database()
     insert_sample_data()
+      
+      
+    
   except sqlite3.Error as e:
     print(e)
