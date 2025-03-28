d268374a3ef895e07abb6a55c393f4beee7aef0a
Ryan Milligan <rcm9@sfu.ca>
2025-03-28 14:57:21 -0700

    add more instances

--- sample_data/ItemInstance.sql
+++ sample_data/ItemInstance.sql
@@ -1,17 +1,65 @@
 INSERT INTO ItemInstance (itemId, instanceId) 
 VALUES
-  (1,  1), -- The Great Gatsby
-  (1,  2), -- The Great Gatsby
-  (2,  1), -- Inception
-  (3,  1), -- National Geographic - July Edition
-  (4,  1), -- Python Programming
-  (5,  1), -- Beethoven Symphony No.9
-  (6,  1), -- To Kill a Mockingbird
-  (7,  1), -- The Lord of the Rings
-  (8,  1), -- 1984
-  (9,  1), -- The Matrix
-  (10, 1), -- Interstellar
-  (11, 1), -- Dune
-  (12, 1), -- The Catcher in the Rye
-  (13, 1), -- Cosmos
-  (14, 1); -- The Art of War
+  -- The Great Gatsby
+  (1,  1), 
+  (1,  2),
+  (1,  3),
+
+  -- Inception
+  (2,  1), 
+  (2,  2), 
+  
+  
+   -- National Geographic - July Edition
+  (3,  1),
+  
+  -- Python Programming
+  (4,  1), 
+  (4,  2), 
+  (4,  3), 
+  (4,  4), 
+  
+  -- Beethoven Symphony No.9
+  (5,  1), 
+  (5,  2), 
+  
+  -- To Kill a Mockingbird
+  (6,  1), 
+  (6,  2), 
+  (6,  3), 
+  (6,  4), 
+  
+  -- The Lord of the Rings
+  (7,  1), 
+  (7,  2), 
+  (7,  3), 
+  (7,  4), 
+  
+   -- 1984
+  (8,  1),
+  (8,  2),
+  (8,  3),
+  
+  -- The Matrix
+  (9,  1), 
+  (9,  2), 
+  (9,  3), 
+  
+  -- Interstellar
+  (10, 1), 
+  (10, 2), 
+  
+  -- Dune
+  (11, 1), 
+  (11, 2), 
+  
+  -- The Catcher in the Rye
+  (12, 1), 
+  (12, 2), 
+  (12, 3), 
+  
+  -- Cosmos
+  (13, 1), 
+  
+   -- The Art of War
+  (14, 1);
--- schemas/ItemInstance.sql
+++ schemas/ItemInstance.sql
@@ -4,7 +4,13 @@ CREATE TABLE IF NOT EXISTS ItemInstance (
     currentCheckoutId INTEGER UNIQUE DEFAULT NULL, -- nullable, because the item may not be currently checked out.
     -- Unique because no two ItemInstances should have the same CheckoutRecord.
 
-    PRIMARY KEY (itemId, instanceId)
-    FOREIGN KEY (itemId) REFERENCES Item(itemId),
-    FOREIGN KEY (currentCheckoutId) REFERENCES CheckoutRecord(checkoutId)
+    PRIMARY KEY (itemId, instanceId),
+    FOREIGN KEY (itemId) 
+        REFERENCES Item(itemId)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE,
+    FOREIGN KEY (currentCheckoutId) 
+        REFERENCES CheckoutRecord(checkoutId)
+        ON UPDATE CASCADE
+        ON DELETE SET NULL
 );
