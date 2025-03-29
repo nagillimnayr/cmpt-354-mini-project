cfdcdae97f5b46f6763431199e35e973745f2f57
Ryan Milligan <rcm9@sfu.ca>
2025-03-29 15:02:07 -0700

    fix import

--- db_functions/__init__.py
+++ db_functions/__init__.py
@@ -5,4 +5,5 @@ from db_functions.personnel import *
 from db_functions.checkout_records import *
 from db_functions.fines import *
 from db_functions.events import *
+from db_functions.forum import *
   
